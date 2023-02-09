2a2944df396cd57373de0656a52217861234a136
Bastien <Udshoft@gmail.com>
2023-02-09 12:17:34 +0100

    MaJ des routes

--- models/users.js
+++ models/users.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const tweetSchema = mongoose.Schema({
-    date:Date,
-    tweetContent:String,
-    likes:Number,
-})
+	date: Date,
+	tweetContent: String,
+	likes: Number,
+});
 
 const usersSchema = mongoose.Schema({
-    firstName:String,
-    username:String,
-    password:String,
-    tweet:[tweetSchema],
+	firstname: String,
+	username: String,
+	password: String,
+	token: String,
+	tweets: [tweetSchema],
+});
 
-})
-
-const User = mongoose.model('users',usersSchema);
+const User = mongoose.model('users', usersSchema);
 
 module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,48 @@
 var express = require('express');
 var router = express.Router();
-const User = require('../models/users')
-/* GET users listing. */
-router.post('/signup', function(req, res, next) {
-  const newUser = new User({
-    firstName:req.body.firstName,
-    username:req.body.username,
-    password:req.body.password,
-    tweet:[{date:Date.now(),tweetContent:req.body.tweetContent,likes:req.body.likes}],
-  })
-  newUser.save().then(()=>res.send('respond with a resource'))
-  
+const User = require('../models/users');
+const bcrypt = require('bcrypt');
+const uid2 = require('uid2');
+
+/* SIGN UP A USER */
+router.post('/signup', function (req, res) {
+	if (req.body.username === '' || req.body.firstname === '' || req.body.password === '') {
+		res.json({ result: false, error: 'Missing or Empty fields' });
+		return;
+	}
+
+	User.findOne({ username: req.body.username }).then((data) => {
+		if (data === null) {
+			const hash = bcrypt.hashSync(req.body.password, 10);
+			const newUser = new User({
+				firstname: req.body.firstname,
+				username: req.body.username,
+				password: hash,
+				token: uid2(32),
+				tweets: [{ date: Date.now(), tweetContent: '', likes: 0 }],
+			});
+			newUser.save().then((data) => res.json({ result: true, data: data }));
+		} else {
+			res.json({ result: false, error: 'User is already registered' });
+		}
+	});
 });
 
 module.exports = router;
+
+/* SIGN IN ALREADY EXISTING USER */
+
+router.post('/signin', (req, res) => {
+	if (req.body.username === '' || req.body.password === '') {
+		res.json({ result: false, error: 'Missing or empty fields' });
+		return;
+	}
+
+	User.findOne({ username: req.body.username }).then((data) => {
+		if (data && bcrypt.compareSync(req.body.password, data.password)) {
+			res.json({ result: true, token: data.token });
+		} else {
+			res.json({ result: false, error: 'User not found / Wrong creditentials' });
+		}
+	});
+});
