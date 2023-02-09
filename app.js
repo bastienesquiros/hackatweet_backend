effe1d9525b272fe221625349d31c1470a84905e
Bastien <Udshoft@gmail.com>
2023-02-09 14:08:04 +0100

    MaJ app

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var path = require('path');
+const cors = require('cors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('./models/connection')
+require('./models/connection');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -13,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
