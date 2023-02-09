d90f8d09dc9a9ff8ff858b8fb04a5d44ffa9ed19
theostone62 <theostone62@gmail.com>
2023-02-09 10:41:09 +0100

    back 10:41

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # hackatweet_backend
+# CHANGE PAGE
+ window.location.href = '/home';
+
+ # SOUS DOCUMENTS
+const addressSchema = mongoose.Schema({
+ street: String,
+ city: String,
+});
+
+const userSchema = mongoose.Schema({
+ firstName: String,
+ lastName: String,
+ email: String,
+ address: addressSchema,
+});
+
+const User = mongoose.model('users', userSchema);
--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
-const connectionString = ''
+const connectionString = 'mongodb+srv://hackatweet:motdepasse@hackatweet.uzkzhoy.mongodb.net/hackatweet';
+console.log(connectionString)
 
-mongoose.connect(conenctionString, {connectTimeoutMS:2000})
+mongoose.connect(connectionString, {connectTimeoutMS:2000})
     .then(()=>console.log('Database connected'))
     .catch(error=>console.error(error))
