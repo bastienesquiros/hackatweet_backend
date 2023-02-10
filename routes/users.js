32a498c9013a4d3ee8c91a1d5ccf5d9a1f123b36
Bastien <Udshoft@gmail.com>
2023-02-10 17:24:10 +0100

    Maj Route

--- routes/users.js
+++ routes/users.js
@@ -45,45 +45,46 @@ router.post('/signin', (req, res) => {
 	});
 });
 
-router.post('/addTweet',(req,res)=>{
-	const hashtags = getHashTags(req.body.tweetContent)
-	User.updateOne(
-		{username:req.body.username},
-		{$push:{tweets:[{ date: Date.now(), tweetContent:req.body.tweetContent, likes: 0,hashtags:hashtags}]}}
-	).then(data=>res.json({result:true}))
-})
+router.post('/addTweet', (req, res) => {
+	const hashtags = getHashTags(req.body.tweetContent);
+	User.updateOne({ username: req.body.username }, { $push: { tweets: [{ date: Date.now(), tweetContent: req.body.tweetContent, likes: 0, hashtags: hashtags }] } }).then((data) => res.json({ result: true }));
+});
 
-router.get('/getTweetsByUserName/:username',(req,res)=>{
-	User.findOne({username:req.params.username})
-		.then(data=>res.json({result:true,userData:{username:data.username,firstname:data.firstname,token:data.token},tweets:data.tweets.filter(obj=>obj.tweetContent.trim())}))
+router.get('/getTweetsByUserName/:username', (req, res) => {
+	User.findOne({ username: req.params.username }).then((data) => res.json({ result: true, userData: { username: data.username, firstname: data.firstname, token: data.token }, tweets: data.tweets.filter((obj) => obj.tweetContent.trim()) }));
+});
+router.delete('/deleteAll', (req, res) => {
+	User.deleteMany().then(res.json({ result: true }));
+});
 
-})
-router.delete('/deleteAll',(req,res)=>{
-	User.deleteMany().then(res.json({result:true}))
-})
-
-router.get('/getAllTweets',(req,res)=>{
-	User.find().then((data)=>{
+router.get('/getAllTweets', (req, res) => {
+	User.find().then((data) => {
 		const transformedData = [];
 
 		for (const user of data) {
 			for (const tweet of user.tweets) {
 				transformedData.push({
-				username: user.username,
-				firstname: user.firstname,
-				date: tweet.date,
-				tweetContent: tweet.tweetContent,
-				hashtags: tweet.hashtags,
-				likes: tweet.likes,
-				_id: tweet._id
+					username: user.username,
+					firstname: user.firstname,
+					date: tweet.date,
+					tweetContent: tweet.tweetContent,
+					hashtags: tweet.hashtags,
+					likes: tweet.likes,
+					_id: tweet._id,
 				});
 			}
 		}
-		const dataSorted = transformedData.sort((a,b)=>{
-			return new Date(b.date) - new Date(a.date)
-		})
-		res.json({allTweets:dataSorted})
-	})
-})
+		const dataSorted = transformedData.sort((a, b) => {
+			return new Date(b.date) - new Date(a.date);
+		});
+		res.json({ allTweets: dataSorted });
+	});
+});
+
+router.post('/delete', (req, res) => {
+	User.findOne({ username: req.body.username }).then((response) => {
+		res.json({ data: response.tweets });
+	});
+});
 
 module.exports = router;
