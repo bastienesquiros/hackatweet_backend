d902df66cc118c4c01e357cf367f52ef2682593f
Bastien <Udshoft@gmail.com>
2023-02-10 09:15:29 +0100

    MaJ des erreurs

--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,17 @@ var router = express.Router();
 const User = require('../models/users');
 const bcrypt = require('bcrypt');
 const uid2 = require('uid2');
-const  {getHashTags} = require('../modules/getHashTags')
+const { getHashTags } = require('../modules/getHashTags');
 
 /* SIGN UP A USER */
 router.post('/signup', function (req, res) {
-	if (req.body.username === '' || req.body.firstname === '' || req.body.password === '') {
+	if (req.body.username.trim() === '' || req.body.firstname.trim() === '' || req.body.password.trim() === '') {
 		res.json({ result: false, error: 'Missing or Empty fields' });
 		return;
 	}
 
 	User.findOne({ username: req.body.username }).then((data) => {
 		if (data === null) {
-
 			const hash = bcrypt.hashSync(req.body.password, 10);
 			const newUser = new User({
 				firstname: req.body.firstname,
@@ -28,7 +27,6 @@ router.post('/signup', function (req, res) {
 		}
 	});
 });
-
 
 /* SIGN IN ALREADY EXISTING USER */
 
@@ -47,22 +45,16 @@ router.post('/signin', (req, res) => {
 	});
 });
 
-router.post('/addTweet',(req,res)=>{
-	const hashtags = getHashTags(req.body.tweetContent)
-	User.updateOne(
-		{username:req.body.username},
-		{$push:{tweets:[{ date: Date.now(), tweetContent:req.body.tweetContent, likes: 0,hashtags:hashtags}]}}
-	).then(data=>res.json({result:true}))
-})
+router.post('/addTweet', (req, res) => {
+	const hashtags = getHashTags(req.body.tweetContent);
+	User.updateOne({ username: req.body.username }, { $push: { tweets: [{ date: Date.now(), tweetContent: req.body.tweetContent, likes: 0, hashtags: hashtags }] } }).then((data) => res.json({ result: true }));
+});
 
-router.get('/getTweets',(req,res)=>{
-	User.findOne({username:req.body.username})
-		.then(data=>res.json({result:true,userData:{username:data.username,firstname:data.firstname,token:data.token},tweets:data.tweets.filter(obj=>obj.tweetContent.trim())}))
-
-})
-router.get('/deleteAll',(req,res)=>{
-	User.deleteMany().then(res.json({result:true}))
-
-})
+router.get('/getTweets', (req, res) => {
+	User.findOne({ username: req.body.username }).then((data) => res.json({ result: true, userData: { username: data.username, firstname: data.firstname, token: data.token }, tweets: data.tweets.filter((obj) => obj.tweetContent.trim()) }));
+});
+router.get('/deleteAll', (req, res) => {
+	User.deleteMany().then(res.json({ result: true }));
+});
 
 module.exports = router;
